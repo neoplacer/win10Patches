eeb4d49b7c7478e73f087f079c49b03c7fc5e592
Richard Gericke <neoplacer@4rmb.de>
2016-07-29 14:32:16 +0200

    remove-storeapps.ps1 - Entfernt die MS Storeapps removeOnedrive.cmd - Entfernt die Cloud

---
+++ remove-storeapps.ps1
@@ -0,0 +1,19 @@
+Get-AppxPackage -name "Microsoft.XboxAPP" | Remove-AppxPackage
+Get-AppxPackage *windowsstore | Remove-AppxPackage
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.ZuneMusic" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.BingTravel" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.BingHealthAndFitness" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.BingFoodAndDrink" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.BingFinance" | Remove-AppxPackage
+
+Get-AppxPackage -name "Microsoft.AAD.BrokerPlugin" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.Windows.Cortana" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.XboxGameCallableUI" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.XboxIdentityProvider" | Remove-AppxPackage
+Get-AppxPackage -name "Windows.PurchaseDialog" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.BingNews" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.BingWeather" | Remove-AppxPackage
+Get-AppxPackage -name "Microsoft.BingSports" | Remove-AppxPackage
+Get-AppxPackage -name "9E2F88E3.Twitter" | Remove-AppxPackage
+Get-AppxPackage -name "king.com.CandyCrushSodaSaga" | Remove-AppxPackage
